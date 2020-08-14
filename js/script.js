bc9d24092fdfd5117cfd822846c89b688abff358
Jose Grimaldo <juan.grimaldovl@gmail.com>
2020-08-14 02:57:28 -0500

    Se añade funcion completa para detectar la seccion activa en NAV.BAR

--- js/script.js
+++ js/script.js
@@ -4,10 +4,34 @@ window.onscroll = function() { myFunction() };
 // Get the navbar
 var navbar = document.getElementById("navbar");
 var hero = document.getElementById("hero");
-var fritz = document.getElementById("prueba");
+
+var fritz_h1 = document.getElementById("fritz");
+var menu_h1 = document.getElementById("menu");
+var awards_h1 = document.getElementById("awards");
+var reviews_h1 = document.getElementById("reviews");
+var location_h1 = document.getElementById("location");
+var contact_h1 = document.getElementById("contact");
+
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
+
+
+//Get the offset position of active section
+var fritz_top = fritz_h1.offsetTop;
+var menu_top = menu_h1.offsetTop;
+var awards_top = awards_h1.offsetTop;
+var reviews_top = reviews_h1.offsetTop;
+var location_top = location_h1.offsetTop;
+var contact_top = contact_h1.offsetTop;
+
+var fritz_id = document.getElementById("fritz_nav");
+var menu_id = document.getElementById("menu_nav");
+var awards_id = document.getElementById("awards_nav");
+var reviews_id = document.getElementById("reviews_nav");
+var location_id = document.getElementById("location_nav");
+var contact_id = document.getElementById("contact_nav");
+
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
@@ -20,5 +44,53 @@ function myFunction() {
         navbar.classList.remove("sticky")
         navbar.classList.remove("sticky-add")
         hero.classList.remove("fix-height")
+    }
+
+    if (window.pageYOffset >= fritz_top) {
+        fritz_id.classList.add("active-nav")
+    } else {
+        fritz_id.classList.remove("active-nav")
+    }
+
+    if (window.pageYOffset + 40 >= menu_top) {
+        menu_id.classList.add("active-nav")
+        fritz_id.classList.remove("active-nav")
+    } else {
+        menu_id.classList.remove("active-nav")
+    }
+
+    if (window.pageYOffset + 40 >= awards_top) {
+        awards_id.classList.add("active-nav")
+        fritz_id.classList.remove("active-nav")
+        menu_id.classList.remove("active-nav")
+    } else {
+        awards_id.classList.remove("active-nav")
+    }
+    if (window.pageYOffset + 40 >= reviews_top) {
+        reviews_id.classList.add("active-nav")
+        fritz_id.classList.remove("active-nav")
+        menu_id.classList.remove("active-nav")
+        awards_id.classList.remove("active-nav")
+    } else {
+        reviews_id.classList.remove("active-nav")
+    }
+    if (window.pageYOffset + 40 >= location_top) {
+        location_id.classList.add("active-nav")
+        fritz_id.classList.remove("active-nav")
+        menu_id.classList.remove("active-nav")
+        awards_id.classList.remove("active-nav")
+        reviews_id.classList.remove("active-nav")
+    } else {
+        location_id.classList.remove("active-nav")
+    }
+    if (window.pageYOffset + 40 >= contact_top) {
+        contact_id.classList.add("active-nav")
+        fritz_id.classList.remove("active-nav")
+        menu_id.classList.remove("active-nav")
+        awards_id.classList.remove("active-nav")
+        reviews_id.classList.remove("active-nav")
+        location_id.classList.remove("active-nav")
+    } else {
+        contact_id.classList.remove("active-nav")
     }
 }
